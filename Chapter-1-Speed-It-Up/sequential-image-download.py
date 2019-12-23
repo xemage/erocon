2a6209d68564ea2f1149fe92a32ff35070c46f52
emage <emage@email.de>
2019-12-23 10:38:15 +0100

    Added time measurement to sequential image download

--- Chapter-1-Speed-It-Up/sequential-image-download.py
+++ Chapter-1-Speed-It-Up/sequential-image-download.py
@@ -2,6 +2,7 @@
 # from lorempixel which will deliver a different image each call
 
 import urllib.request
+import time
 from pathlib import Path
 
 def downloadImage(url, file):
@@ -9,9 +10,16 @@ def downloadImage(url, file):
     urllib.request.urlretrieve(url, file)
 
 def main():
+    t0 = time.time()
+
     for i in range(10):             
         image_path = Path("temp/image-{0}.jpg".format(str(i)))
         downloadImage("http://lorempixel.com/400/200/sports", image_path)
+
+    # calculate the total execution time
+    t1 = time.time()
+    totalTime = t1 - t0
+    print("Total Execution Time {}".format(totalTime))
 
 if __name__ == '__main__':
     main()
