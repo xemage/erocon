22f3e588dcc802bbbe5992706b61a7bbd55a463e
emage <emage@email.de>
2019-12-27 15:40:29 +0100

    Added rlocks.py

---
+++ Chapter-4-Synchronization-Between-Threads/rlocks.py
@@ -0,0 +1,33 @@
+import threading
+import time
+
+class myWorker():
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+        self.Rlock = threading.RLock()
+
+    def modifyA(self):
+        with self.Rlock:
+            print("Modifying A : RLock Acquired: {0}".format(self.Rlock._is_owned()))
+            print("{0}".format(self.Rlock))
+            self.a = self.a + 1
+            time.sleep(5)
+
+    def modifyB(self):
+        with self.Rlock:
+            print("Modifying B : RLock Acquired: {0}".format(self.Rlock._is_owned()))
+            print("{0}".format(self.Rlock))
+            self.b = self.b - 1
+            time.sleep(5)
+
+    def modifyBoth(self):
+        with self.Rlock:
+            print("Rlock acquired, modifying A and B")
+            print("{0}".format(self.Rlock))
+            self.modifyA()
+            self.modifyB()
+            print("{0}".format(self.Rlock))
+
+workerA = myWorker()
+workerA.modifyBoth()
